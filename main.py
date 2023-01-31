432ca8c598b72561dfb24d3b70f2582a395f6a44
matt <matthewprovank@gmail.com>
2023-01-31 13:24:52 +1100

    test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import name_generator
 app = Flask(__name__)
 
 
-@app.route("/names")
+@app.route("/")
 def names():
     generated_names = name_generator.generate_name_list()
     return generated_names
